fb896c65cf1f18b5f9ff039aa7270e0ff0b30075
Fred Small <Fsmall@startoffsmall.com>
2015-12-05 19:38:25 -0500

    Checkpoint save

--- src/Main.java
+++ src/Main.java
@@ -14,32 +14,34 @@ public class Main {
 		System.out.println("Running ProcessBuilder\n\n");
 		
 		String program = "";
-		boolean returnImmediate = false;
+		boolean blockingCall = false;
 		
-		if (args.length == 1)
+		if (args.length >= 1)
 		{
-			if (!args[0].endsWith("&"))
+			if (args[0].endsWith("&"))
 			{
-				program=args[0]; 
+				program=args[0].substring(0, args[0].length()-1);
+				blockingCall = true;
 			}
 			else
 			{
-				program=args[0].substring(0, args[0].length()-1);
-				returnImmediate = true;
+				program=args[0];				
 			}
 		}
-		
+				
     	try {
-			ProcessBuilder processBuilder = new ProcessBuilder("notepad");
+			ProcessBuilder processBuilder = new ProcessBuilder(program);
 			
 			
 			try {
 				Process process = processBuilder.start();
-				if (program.endsWith("&"))
+				if (blockingCall)
+				{
+					System.out.println("Waiting for main thread to terminate");
 					process.waitFor();
-				
+				}
 			} catch (IOException | InterruptedException e) {
-				e.printStackTrace();
+				System.out.println(e.getMessage());
 			}
 			
     	}
