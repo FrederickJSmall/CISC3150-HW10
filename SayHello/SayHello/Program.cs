26bd0fa38ad6cb48d82574d53447db877ef9896f
Fred Small <Fsmall@startoffsmall.com>
2015-12-07 23:47:33 -0500

    add source code for sayhello

---
+++ SayHello/SayHello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(System.DateTime.Now);
+                Console.WriteLine("Hello World");
+                //System.Threading.Thread.Sleep(500);
+            }
+            System.Environment.Exit(0);
+        }
+    }
+}
